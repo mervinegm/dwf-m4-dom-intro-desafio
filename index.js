a9f037348f612b1017125090fb10fd4b29b494d6
Mervin González <gonzalez.mervin@gmail.com>
2023-05-02 16:24:44 -0300

    segundo commit

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const parentEl = document.querySelector(".lista");
+  console.log(parentEl);
+
+  const arrayEl = document.querySelectorAll("li");
+
+  for (let index = 0; index < arrayEl.length; index++) {
+    parentEl.removeChild(arrayEl[index]);
+  }
+
+  for (let index = 0; index < cosasQueAprendimos.length; index++) {
+    const newEL = document.createElement("li");
+    parentEl.appendChild(newEL);
+    newEL.textContent = cosasQueAprendimos[index].tema;
+    if (cosasQueAprendimos[index].class != "") {
+      newEL.classList.add(cosasQueAprendimos[index].class);
+    }
+  }
+}
 
 main();
